1b8fcf1fb7b4c9eeb6c89164d3d7e592367c721f
marshalonah <marshalonah@gmail.com>
2022-04-21 22:41:09 +0100

    platform movement

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private float Speed = 50f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        MoveVertically();
+    }
+
+    public void MoveVertically()
+    {
+        //transform.Translate(Vector2.up * Speed * Time.deltaTime);
+        //transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x, 5.1f), Speed * Time.deltaTime);
+        rb.velocity = Vector2.up * Speed * Time.deltaTime;
     }
 }
