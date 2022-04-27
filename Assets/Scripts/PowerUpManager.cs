a20805b922e6cac7a63bdcdb007536d3fce99c0e
marshalonah <marshalonah@gmail.com>
2022-04-27 15:33:46 +0100

    implemented ForceField PowerUp

--- Assets/Scripts/PowerUpManager.cs
+++ Assets/Scripts/PowerUpManager.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PowerUpManager : MonoBehaviour
 {
+    [Header("Prefabs")]
+    public GameObject ForceField;
+
     public static PowerUpManager instance = null;
+    public bool isShieldActive = false;
 
     private void Awake()
     {
@@ -23,21 +27,27 @@ public class PowerUpManager : MonoBehaviour
 
     }
 
-    //-------------Multiplies the Score of the Player---------------//
+    //-------------Multiplies the Score of the Player for 15 seconds---------------//
     public IEnumerator ScoreMultiplier(int Score)
     {
         int timer = 15;
         while (timer > 0)
         {
-            Score += 100;
+            Score += 200;
             timer--;
             Debug.Log(Score);
             yield return new WaitForSeconds(1f);
         }
     }
 
-    public void Shield()
+    //------------------Activates a forceField around the player for 5 seconds-----------------//
+    public IEnumerator ActivateShield(Transform player)
     {
+        GameObject forceField = Instantiate(ForceField, player.position, transform.rotation, player);
+        isShieldActive = true;
 
+        yield return new WaitForSeconds(5f);
+        isShieldActive = false;
+        Destroy(forceField);
     }
 }
